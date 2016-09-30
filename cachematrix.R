9a6d10a4308b098a45eee9d04e856f3285d5c01b
SunitaAkkapeddi <akkapeddi.sunita87@gmail.com>
2016-09-29 23:02:33 -0500

    Updated code
    Added logic for calculating inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## mInv - variable for Calculated Inverse Matrix
+  mInv <- NULL
+  set <- function(b) {
+    x <<- b
+    mInv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) mInv <<- solve
+  getinverse <- function() mInv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +23,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mInv <- x$getinverse()
+  ## Check for cached value
+  if(!is.null(mInv)) {
+    message("Inverse calculated reading from cached data")
+    return(mInv)
+  }
+  ## calculate inverse
+  data <- x$get()
+  mInv <- solve(data,...)
+  x$setinverse(mInv)
+  return(mInv)
 }
+
+
